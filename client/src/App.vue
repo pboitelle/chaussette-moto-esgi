a8b71e92d85f06f5a280a3af092676c47359fa7c
victor <valee.victor@gmail.com>
2023-01-31 23:09:52 +0100

    chat websocket connetion try 1

--- client/src/App.vue
+++ client/src/App.vue
@@ -12,39 +12,53 @@ import io from 'socket.io-client'
 var socket = io("http://localhost:9000");
 
 const showModal = ref(false)
-var input = document.getElementById('input');
 
 const openModal = () => {
   showModal.value = !showModal.value
-  // socket.emit('connection');
-  // le ligne ci-dessous permet de se connecter au serveur socket.io et de recevoir les messages du serveur socket.io
-  // "open" est un événement qui est émis par socket.io quand la connexion est établie avec le serveur socket.io
-  socket.addEventListener("open", () => {
-    console.log('Message from server ');
-    // send a message to the server
-    // socket.send(JSON.stringify({
-    //   type: "hello from client",
-    //   content: [ 3, "4" ]
-    // }));
-    if (input.value) {
-      socket.emit('message', input.value);
-      console.log('Message from server ', data);
-      input.value = '';
+  var input = document.getElementById('input');
+  var button = document.getElementById('button');
+  var modal = document.getElementById('modal');
+  // quand j'ouvre la modal je veux qu'un message soit envoyé au serveur
+  // socket.emit('message', text);
+
+  socket.on("messageConseiller", ({data}) => {
+    console.log(data);
+    const messages = document.getElementById('modal')
+
+    if (!messages) {
+        throw new Error("Messages container not found")
     }
+
+    const messageElement = document.createElement('div')
+    messageElement.innerText = `${text}`
+    messages.appendChild(messageElement)
+  })
+  
+  button.addEventListener('click', function() {
+    const text = input.value;
+    socket.emit('message', text);
+
+    // création d'un élément div pour afficher le message de l'utilisateur dans la modal qui a pour id="modal"
+    var div = document.createElement('div');
+    div.innerHTML = text;
+    modal.appendChild(div);
+    input.value = '';
+
   });
 
-  // receive a message from the server
-  socket.addEventListener("message", ({ data }) => {
+  // recupere le message du server dans server.js qui correspond a la variable response et affiche la en créant un élément div dans la modal
+  socket.on("response", (response) => {
+    console.log(response);
+    const messages = document.getElementById('modal')
 
-  console.log('Message from server ', data);
-  const packet = JSON.parse(data);
+    if (!messages) {
+        throw new Error("Messages container not found")
+    }
 
-  switch (packet.type) {
-    case "hello from server":
-      // ...
-      break;
-  }
-  });
+    const messageElement = document.createElement('div')
+    messageElement.innerText = `${response}`
+    messages.appendChild(messageElement)
+  })
 }
 
 const closeModal = () => {
@@ -68,8 +82,16 @@ const closeModal = () => {
   </div>
   
   <Modal :showModal="showModal" title="Vieux motard que jamais !" :close="closeModal">
+    <div id="modal">
+      <p>Salut, je suis le chatbot de l'application. Je suis là pour t'aider à trouver des motards près de chez toi.</p>
+      <p>Comment je peux t'aider ?</p>
+      <p>Je souhaiterais des l'entretien sur mon véhicule</p>
+      <p>Si le client souhaite des informations sur les véhicules</p>
+      <p>Si le client souhaite des informations de contact</p>
+      <p>Si le client souhaite arrêter le workflow</p>
+    </div>
     <input v-model="text" type="text" id="input">
-    <button v-on:click="onclick" name="button"></button>
+    <button v-on:click="onclick" id="button"></button>
   </Modal>
   
 </template>
--- server/server.js
+++ server/server.js
@@ -125,24 +125,29 @@ io.on("connection", (socket) => {
 
     // receive a message from the client
   socket.on('message', (data) => {
-    console.log('Message from server ', data);
-    // const packet = JSON.parse(data);
-
-    // switch (packet.type) {
-    //   case "hello from client":
-    //     // ...
-    //     break;
-    // }
-
+    // console.log('Message from server ', data);
     switch (true) {
       case data === "Je souhaiterais des informations sur les véhicules":
-        console.log("Oui, pour quel usage souhaitez-vous en faire ?");
-        console.log(data);
-        socket.emit('reponse', "Quel est l'usage que vous souhaitez pour votre véhicule (routier, tout-terrain, sportif, etc.)?");
-        break;
-      case text.value.includes(data):
-        // Traitez la question de l'utilisateur ici
-        break;
-    }
+      var response = "Quel est le type d'usage que vous souhaitez pour votre véhicule ?";
+      socket.emit('response', response);
+      break;
+      case data.includes("usage routier"):
+      var response = "Nous vous proposons un essai routier. Les rendez-vous routiers sont isolés des autres rendez-vous.";
+      socket.emit('response', response);
+      break;
+      case data.includes("usage tout-terrain"):
+      var response = "Nous vous proposons un essai tout-terrain. Les rendez-vous tout-terrain sont isolés des autres rendez-vous.";
+      socket.emit('response', response);
+      break;
+      case data.includes("usage sportif"):
+      var response = "Nous vous proposons un essai sur piste. Les rendez-vous piste sont isolés des autres rendez-vous.";
+      socket.emit('response', response);
+      break;
+      }
+      
+      
+      
+      
+      
   });
 })
